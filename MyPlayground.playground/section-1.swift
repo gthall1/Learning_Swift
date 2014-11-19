3de996e0e2ec0e3d67870f9a58b660bc7f489375
Griff Hall <griffhall1@gmail.com>
2014-11-19 16:56:11 -0600

    added all variables, simple numerical operations

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -2,6 +2,67 @@
 
 import UIKit
 
+// Variable Types
+
+/*
+This
+is
+a
+multi line
+comment
+*/
+
 var str = "Hello, playground"
 
 str = "Hello World"
+
+var tree = "Oak"
+
+var nomar = 5
+
+var age = 27
+
+var pi = 3.14
+
+var e = 2.71
+
+var year:Int
+
+year = 2014
+
+var son:String = "Tom"
+
+var pizzas:Float = 5
+pizzas = 1.2
+
+var a = 4
+var b = 3
+
+var c = a + b
+
+var d = a * b
+
+var weird = str + " " + tree
+
+var newString = "My name is " + son
+
+var finalString = "My age is \(age)"
+
+var name = "Griff"
+
+var number = 216
+
+var secondNumber = 562
+
+var thirdNumber = number * secondNumber
+
+
+var quiz = "My name is \(name) and \(number) * \(secondNumber) is \(thirdNumber)"
+
+
+
+
+
+
+
+
